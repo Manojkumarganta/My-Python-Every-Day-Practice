1101db472bbaf1391ab845f85f529acf8e6649e6
Manojkumargamta <manuganta9908@gmail.com>
2025-09-17 21:16:17 +0530

    example on class

---
+++ class.py
@@ -0,0 +1,15 @@
+class student:
+    pass
+
+class human :
+    pass
+
+obj = student()
+print(obj)
+print(type(obj))
+print(id(obj))
+ 
+obj2 = human()
+print(obj2)
+print(type(obj2))
+print(id(obj2))
