451e8eca4d5270e2664ffaec83d0dc687d245dba
Manojkumarganta <manuganta9908@gmail.com>
2025-10-02 21:25:53 +0530

    Update Functions1.py

--- Functions1.py
+++ Functions1.py
@@ -1,5 +1,12 @@
 def greet(name):
     print("Hello,", name)
 
+def farewell(name):
+    print("Goodbye,", name)
+
+# Calling functions
 greet("Alice")
 greet("Bob")
+
+farewell("Alice")
+farewell("Bob")
